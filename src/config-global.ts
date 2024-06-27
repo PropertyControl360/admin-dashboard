7712bdd33d69853174b889868c0ce71759402495
Bereket Tilahun <btilahun287@gmail.com>
2024-06-27 18:19:37 +0300

    UAT-PROD

--- src/config-global.ts
+++ src/config-global.ts
@@ -4,6 +4,7 @@ import { paths } from 'src/routes/paths';
 // ----------------------------------------------------------------------
 
 export const HOST_API = import.meta.env.VITE_HOST_API;
+export const HOST_PROD_API = import.meta.env.VITE_PROD_HOST_API;
 export const ASSETS_API = import.meta.env.VITE_ASSETS_API;
 
 export const FIREBASE_API = {
